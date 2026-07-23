0df0ee2870dca4fe59100d762e93520897df1b65
jmardente <JMARDENTE@OUTLOOK.COM>
2026-07-23 11:34:45 -0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-
-const toggle=document.querySelector('.menu-toggle');
-const menu=document.querySelector('.menu');
-if(toggle){toggle.addEventListener('click',()=>{menu.classList.toggle('open');toggle.setAttribute('aria-expanded',menu.classList.contains('open'));});}
