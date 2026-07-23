f506fd7fb3282366c6fefe3489b87f6b55af897e
jmardente <JMARDENTE@OUTLOOK.COM>
2026-07-22 22:23:19 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-const menu=document.querySelector('.menu-toggle');
-const nav=document.querySelector('.icon-nav');
-if(menu&&nav){menu.addEventListener('click',()=>{nav.classList.toggle('open');menu.setAttribute('aria-expanded',nav.classList.contains('open'))})}
-document.querySelectorAll('#year').forEach(el=>el.textContent=new Date().getFullYear());
