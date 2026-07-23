46fb50bea6b28aa0f2374605841ed3b273f50d8c
jmardente <JMARDENTE@OUTLOOK.COM>
2026-07-23 12:06:37 -0700

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -5,7 +5,7 @@
   https://buy.stripe.com/xxxxxxxxxxxxxxxxxxxx
 */
 window.ARDENTE_CONFIG = {
-  stripePaymentLink: "PASTE_YOUR_STRIPE_PAYMENT_LINK_HERE",
+  stripePaymentLink: https://buy.stripe.com/bJeeV50JO9qnbxfbXJcAo00
   facebookUrl: "https://www.facebook.com/",
   amazonBookUrl: "https://www.amazon.com/",
   contactEmail: "YOUR-EMAIL@example.com"
